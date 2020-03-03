48f38a1fbf45d03bf745b9e6432f2233a32be692
Yvon <ym2637@columbia.edu>
2020-03-03 18:54:58 +0300

    created 2 activities and they can chat

---
+++ app/src/main/java/com/example/yvonmanzi/shopping/CommonItemsActivity.java
@@ -0,0 +1,38 @@
+package com.example.yvonmanzi.shopping;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CommonItemsActivity extends AppCompatActivity {
+    public static final String ITEM_EXTRA = "com.example.yvonmanzi.shopping.EXTRA.itemNameId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_items);
+    }
+
+    public void onAddItem(View view) {
+        int viewId = view.getId();
+        int itemId;
+        switch (viewId) {
+            case R.id.button_coffee:
+                itemId = R.string.coffee;
+            case R.id.button_tea:
+                itemId = R.string.tea;
+            case R.id.button_mac:
+                itemId = R.string.mac;
+            default:
+                itemId=R.id.button_coffee;
+        }
+        Intent i = new Intent();
+        i.putExtra(ITEM_EXTRA, itemId);
+        setResult(RESULT_OK, i);
+        finish();
+
+
+
+    }
+}
--- app/src/main/java/com/example/yvonmanzi/shopping/MainActivity.java
+++ app/src/main/java/com/example/yvonmanzi/shopping/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.yvonmanzi.shopping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onAdd(View view) {
+        Intent main_intent = new Intent(this, CommonItemsActivity.class);
+        startActivityForResult(main_intent, REQUEST_CODE);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+         int itemId = data.getIntExtra(CommonItemsActivity.ITEM_EXTRA, R.string.coffee);
+            TextView view = findViewById(R.id.second_item);
+            view.setText(itemId);
+        }
+
+    }
+
 }
